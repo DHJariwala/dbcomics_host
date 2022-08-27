7a97b23bd94f6cff897983b24e66b4fb22d87069
DHJariwala <65482665+DHJariwala@users.noreply.github.com>
2022-08-27 14:11:35 +0530

    change add.py req.txt

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from sqlite3 import SQLITE_OK
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc,select,update
-
+from urllib.request import Request,urlopen
+from bs4 import BeautifulSoup
+import re
 #Config
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abc123'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comics.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pvaarthoxuwbdz:693e223971abf0331755f2499892b898bc38f350e9a10935da1c46642f408f45@ec2-54-225-234-165.compute-1.amazonaws.com:5432/de9odpiav52fde'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comics.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pvaarthoxuwbdz:693e223971abf0331755f2499892b898bc38f350e9a10935da1c46642f408f45@ec2-54-225-234-165.compute-1.amazonaws.com:5432/de9odpiav52fde'
 #no # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mowyjvwjbkafsu:b1e3a478e96a4d66d637317084b249a89c4f1793be43d6ac87c70c66e1bf02c7@ec2-54-225-234-165.compute-1.amazonaws.com:5432/dtbrh26fsnmtp'
 db = SQLAlchemy(app)
 
@@ -36,15 +38,64 @@ class comics(db.Model):
     def __repr__(self):
         return f"comic({self.comic_id},{self.comic_name},{self.comic_chapter_completed})"
 
+class comicsLatestChapter(db.Model):
+    comic_id = db.Column(db.Integer(),primary_key=True,nullable=False)
+    comic_latest_chapter = db.Column(db.Float(),default=0)
+    def __init__(self,comic_id,comic_latest_chapter):
+        self.comic_id = comic_id
+        self.comic_latest_chapter = comic_latest_chapter
+    def __repr__(self):
+        return f"comicLatestChapter({self.comic_id},{self.comic_latest_chapter})"
+
+
 @app.route('/',methods=['GET','POST'])
 def home():
     query = select(comics).order_by(desc(comics.comic_like)).order_by(comics.comic_id)
     comicsData = db.session.execute(query)
+    query = select(comicsLatestChapter)
+    chapterData = db.session.execute(query)
+    chapters = {}
+    for x in chapterData.scalars().all():
+        chapters[x.comic_id] = x.comic_latest_chapter
     if request.method == 'POST':
         comic = request.form['comic']
         comicsData = comics.query.filter(comics.comic_name.like(f"%{comic}%")).order_by(desc(comics.comic_like)).order_by(comics.comic_id).all()
-        return render_template('search.html',comicsData=comicsData)
-    return render_template('search.html',comicsData=comicsData.scalars().all())
+        return render_template('search.html',comicsData=comicsData,chapterData=chapters)
+    return render_template('search.html',comicsData=comicsData.scalars().all(),chapterData=chapters)
+
+@app.route('/refresh/<comicid>')
+def refreshChapter(comicid):
+    comic = comics.query.where(comics.comic_id == comicid).all()[0]
+    url = comic.comic_url
+    comicLatestChap = comicsLatestChapter.query.filter(comicsLatestChapter.comic_id == comicid).all()
+    if comicLatestChap == []:
+        comicExist = 0
+    else:
+        comicExist = 1
+    try:
+        req = Request(url,headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        a = BeautifulSoup(webpage,'lxml')
+        tags = a.find_all('li',class_="wp-manga-chapter")
+        txt = tags[0].a.text
+        x = re.sub('Chapter','',txt)
+        x = float(re.sub('- END','',x))
+        if comicExist:
+            upd = update(comicsLatestChapter)
+            val = upd.values({'comic_latest_chapter':x})
+            cnd = val.where(comicsLatestChapter.comic_id == comicid)
+            db.session.execute(cnd)
+            db.session.commit()
+            return redirect('/')
+        else:
+            comic = comicsLatestChapter(comicid,x)
+            db.session.add(comic)
+            db.session.commit()
+            return redirect('/')
+    except:
+        return redirect('/')
+    
+    
 
 @app.route('/add',methods=['GET','POST'])
 def addPage():
